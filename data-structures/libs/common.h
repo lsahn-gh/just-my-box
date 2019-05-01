8e4c59fa0fa143ddf43cbe59871a829d85893ee3
Leesoo Ahn <yisooan@fedoraproject.org>
2019-05-01 23:27:21 +0900

    common: Add GStack
    It is just an alias of GQueue.

--- data-structures/libs/common.h
+++ data-structures/libs/common.h
@@ -4,3 +4,11 @@
 #include <gmodule.h>
 
 typedef void* npointer;
+
+/* GStack */
+typedef GQueue GStack;
+#define G_STACK_INIT                G_QUEUE_INIT
+#define g_stack_push(stack, value)  g_queue_push_head (stack, value)
+#define g_stack_pop(stack)          g_queue_pop_head (stack)
+#define g_stack_peek(stack)         g_queue_peek_head (stack)
+#define g_stack_is_empty(stack)     g_queue_is_empty (stack)
