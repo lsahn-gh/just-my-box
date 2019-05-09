38b06eded3eb42c974d2e18e1d537e9ef2336f19
Leesoo Ahn <yisooan@fedoraproject.org>
2019-05-10 00:44:00 +0900

    Add additional functions for BST
    - my_tree_find_recur
    - my_tree_find (loop way)
    - my_tree_find_min
    - my_tree_find_max

--- data-structures/Tree/mytree.c
+++ data-structures/Tree/mytree.c
@@ -29,3 +29,65 @@ my_tree_cmp (XZTreeNode *node_a, XZTreeNode *node_b)
         return 0;
 }
 
+MyTreeNode *
+my_tree_find_recur (XZTreeNode *root, int value)
+{
+    npointer ptr;
+
+    if (root == NULL)
+        return NULL;
+
+    if (value == MY_TREE_NODE (root)->data)
+        ptr = root;
+    else if (value < MY_TREE_NODE (root)->data)
+        ptr = my_tree_find (root->left, value);
+    else
+        ptr = my_tree_find (root->right, value);
+    
+    return ptr;
+}
+
+MyTreeNode *
+my_tree_find (XZTreeNode *root, int value)
+{
+    XZTreeNode *cur = root;
+
+    while (cur) {
+        if (value == MY_TREE_NODE (cur)->data)
+            break;
+        else if (value < MY_TREE_NODE (cur)->data)
+            cur = cur->left;
+        else
+            cur = cur->right;
+    }
+
+    return MY_TREE_NODE (cur);
+}
+
+MyTreeNode *
+my_tree_find_min (XZTreeNode *root)
+{
+    XZTreeNode *cur = root;
+
+    if (root == NULL)
+        return NULL;
+
+    while (cur->left)
+        cur = cur->left;
+
+    return MY_TREE_NODE (cur);
+}
+
+MyTreeNode *
+my_tree_find_max (XZTreeNode *root)
+{
+    XZTreeNode *cur = root;
+
+    if (root == NULL)
+        return NULL;
+
+    while (cur->right)
+        cur = cur->right;
+
+    return MY_TREE_NODE (cur);
+}
--- data-structures/Tree/mytree.h
+++ data-structures/Tree/mytree.h
@@ -13,6 +13,10 @@ typedef struct
 
 int my_tree_cmp (XZTreeNode *node_a, XZTreeNode *node_b);
 
-MyTreeNode * my_tree_node_new (int data);
+MyTreeNode * my_tree_node_new   (int data);
+MyTreeNode * my_tree_find       (XZTreeNode *root, int value);
+MyTreeNode * my_tree_find_recur (XZTreeNode *root, int value);
+MyTreeNode * my_tree_find_min   (XZTreeNode *root);
+MyTreeNode * my_tree_find_max   (XZTreeNode *root);
 
 #endif
